1a48b2478d0456db8dbc75ce207fac534661e22d
Shankar Ganesh Purushothaman <puru.shankarg@gmail.com>
2022-05-15 13:13:23 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,32 @@
+package week1.day1;
+
+public class Car {
+	public void applyBreak() {
+		System.out.println("Apply Break");
+		
+	}
+	public void applyGear(){
+		System.out.println("Apply Gear");
+		
+	}
+	public void switchonAc() {
+		System.out.println("SwitchonAc");
+		
+	}
+	public void applyAcclerate() {
+		System.out.println("ApplyAcclerate");
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car carfnc = new Car();
+		
+		carfnc.applyBreak();
+		carfnc.applyGear();
+		carfnc.switchonAc();
+		carfnc.applyAcclerate();
+
+	}
+
+}
